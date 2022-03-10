adc41060ce13a161c783995eef4a4aa2643c0b9a
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-03-10 06:09:54 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
-pass
-pass
+x = 2
+y = 2
+position = [0,0]
+led.plot(x,y)
+list = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+def save():
+    basic.clear_screen()
+    global position
+    position = [x,y]
+    led.plot(x,y)
+    print(position)
+save()
+
+
+
+def on_button_pressed_a():
+    if x > -0:
+        global x
+        x -= 1
+        print(x)
+        save()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if x < 4:
+        global x
+        x += 1
+        print(x)
+        save()
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,5 +1,32 @@
-basic.forever(function on_forever() {
+let x = 2
+let y = 2
+let position = [0, 0]
+led.plot(x, y)
+let list = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+function save() {
+    basic.clearScreen()
+    
+    position = [x, y]
+    led.plot(x, y)
+    console.log(position)
+}
+
+save()
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (x > -0) {
+        
+        x -= 1
+        console.log(x)
+        save()
+    }
     
 })
-
-
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    if (x < 4) {
+        
+        x += 1
+        console.log(x)
+        save()
+    }
+    
+})
