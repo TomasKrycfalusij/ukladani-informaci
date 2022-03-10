a604a3729997543096e9b2c9186471c7f8ebe5bc
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-03-10 08:45:50 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 x = 2
 y = 2
 position = [0,0]
-led.plot(x,y)
-listdat = [0,0]
+led.plot(x, y)
+listdat:List[List[number]] = []
+
+def _x(z, ignored):
+    print(z)
 
 def save():
-    global position, listdat
+    global position
     basic.clear_screen()
-    position = [x,y]
-    led.plot(x,y)
-    print("x = ", x)
+    position = [x, y]
+    led.plot(x, y)
+    listdat.append(position)
+    print("--")
+    listdat.for_each(_x)
+    print("--")
+
 save()
 
-
-
 def on_button_pressed_a():
-    if x > -0:
-        global x
+    global x
+    if x > 0:
         x -= 1
         save()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
+    global x
     if x < 4:
-        global x
         x += 1
         save()
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_event_pressed():
+    global y
+    if y > 0:
+        y -= 1
+        save()
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_pin_pressed_p2():
+    global y
+    if y < 4:
+        y += 1
+        save()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
--- main.ts
+++ main.ts
@@ -2,28 +2,49 @@ let x = 2
 let y = 2
 let position = [0, 0]
 led.plot(x, y)
-let listdat = [0, 0]
+let listdat : number[][] = []
 function save() {
     
     basic.clearScreen()
     position = [x, y]
     led.plot(x, y)
-    console.log("x = ")
+    listdat.push(position)
+    console.log("--")
+    listdat.forEach(function _x(z: any, ignored: any) {
+        console.log(z)
+    })
+    console.log("--")
 }
 
 save()
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    if (x > -0) {
-        
+    
+    if (x > 0) {
         x -= 1
         save()
     }
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (x < 4) {
-        
         x += 1
+        save()
+    }
+    
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    
+    if (y > 0) {
+        y -= 1
+        save()
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
+    if (y < 4) {
+        y += 1
         save()
     }
     
